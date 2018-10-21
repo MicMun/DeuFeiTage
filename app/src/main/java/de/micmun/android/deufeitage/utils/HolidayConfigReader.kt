c33bcf3953236e1d236565864c1b14bfd66955c4
MicMun <MicMunze@gmail.com>
2018-10-21 15:40:06 +0200

    Configuration for holidays with their german states as json files.

---
+++ app/src/main/java/de/micmun/android/deufeitage/utils/HolidayConfigReader.kt
@@ -0,0 +1,63 @@
+/*
+ * HolidayConfigReader.kt
+ *
+ * Copyright 2018 by MicMun
+ */
+package de.micmun.android.deufeitage.utils
+
+import android.content.Context
+import de.micmun.android.deufeitage.R
+import de.micmun.android.deufeitage.model.Holiday
+import de.micmun.android.deufeitage.model.StateItem
+import org.json.JSONArray
+import org.json.JSONObject
+
+/**
+ * Reads the config from json file.
+ *
+ * @author MicMun
+ * @version 1.0, 15.10.18
+ */
+class HolidayConfigReader(context: Context) {
+    private val keyStates = "States"
+    private val keyHolidays = "Holidays"
+    private val keyKey = "KEY"
+    private val keyValue = "VALUE"
+    private val keyId = "ID"
+    private val keyName = "Name"
+    private val keyDesc = "Description"
+
+    val holidays = mutableListOf<Holiday>()
+    val holidayNames = mutableListOf<String>()
+    val states = mutableListOf<StateItem>()
+
+    init {
+        val json = context.resources.openRawResource(R.raw.holidays).bufferedReader()
+                .use { it -> it.readText() }
+        val jsonObject = JSONObject(json)
+        val statesArray = jsonObject[keyStates] as JSONArray
+
+        for (i in 0 until statesArray.length()) {
+            val stateObj = statesArray[i] as JSONObject
+            val state = StateItem(stateObj[keyKey].toString(), stateObj[keyValue].toString())
+            states.add(state)
+        }
+
+        val holidayArray: JSONArray = jsonObject[keyHolidays] as JSONArray
+
+        for (i in 0 until holidayArray.length()) {
+            val holidayObj = holidayArray[i] as JSONObject
+            val holidayStateArr = holidayObj[keyStates] as JSONArray
+            val holidayStates = mutableListOf<String>()
+
+            for (j in 0 until holidayStateArr.length()) {
+                holidayStates.add(holidayStateArr[j].toString())
+            }
+
+            val holiday = Holiday(holidayObj[keyId] as Int, holidayObj[keyName] as String,
+                    holidayStates, holidayObj[keyDesc] as String)
+            holidays.add(holiday)
+            holidayNames.add(holiday.name)
+        }
+    }
+}
